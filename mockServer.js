9261afe53cb964636d2ee42fb935e96cdeb14007
James A <114882053+jAlbright02@users.noreply.github.com>
2025-02-10 14:03:35 +0000

    init commit

---
+++ mockServer.js
@@ -0,0 +1,19 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+
+  //Simulating the STM sending data
+  const interval = setInterval(() => {
+    const mockData = {
+      speed: (Math.random() * 100).toFixed(2),  //Speed between 0-100 randomly
+    };
+    ws.send(JSON.stringify(mockData));
+  }, 1000); //sends every second
+
+  ws.on('close', () => {
+    clearInterval(interval);
+    console.log('Client disconnected');
+  });
+});
